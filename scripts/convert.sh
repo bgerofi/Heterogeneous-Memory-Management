2472b12231a50608299ca523c78b13a0e8e62f3c
Balazs Gerofi <bgerofi@riken.jp>
2021-11-13 19:54:40 +0900

    convert.sh: update

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,13 +3,17 @@
 SCRIPT="`readlink -f ${BASH_SOURCE[0]:-}`"
 SCRIPT_DIR=$(dirname ${SCRIPT})
 
-for f in *.dat; do
-	python3 -u ${SCRIPT_DIR}/pebsview.py -f ${f}
-done
+if [ $# -eq 0 ]; then
+	echo "error: specify input PEBS .dat file"
+	exit 1
+fi
 
-for f in *.feather; do 
+while [ $# -gt 0 ]; do
+	f=$1
+	shift
+	python3 -u ${SCRIPT_DIR}/pebsview.py -f ${f}
+	f="${f%.*}.feather";
 	f_new=`echo ${f} | sed 's/-pid-[0-9]*-tid-[0-9]*//'`
 	mv ${f} ${f_new}
+	echo "${f_new}"
 done
-
-ls *.feather
