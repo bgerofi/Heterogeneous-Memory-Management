8e31d8b1225a118c831905d9eb6202ac9ffa451a
Balazs Gerofi <bgerofi@riken.jp>
2022-01-21 19:52:44 +0900

    Plotter scripts used in ISC'22 submission

--- scripts/plot_csv.py
+++ scripts/plot_csv.py
@@ -14,8 +14,6 @@ if len(sys.argv) != 5:
 sns.set_style("whitegrid")
 
 data = pd.read_csv(sys.argv[1], sep=",", skipinitialspace=True)
-#data = data.set_index("Nodes")
-#data = data.astype({"cycles": float})
 
 data = data[(data["App"] == sys.argv[2]) & (data["Type"] == sys.argv[3])]
 print(data)
@@ -33,32 +31,32 @@ else:
         if data.at[i, "Layout"].startswith("DRAM+HBM-0x"):
             data.at[i, "Layout"] = "DRAM+HBM-" + hashlib.md5(data.at[i, "Layout"].encode()).hexdigest()[0:6]
 
+# Convert to seconds
 for i in data.index:
     data.at[i, "Runtime"] /= 1000
 
 #plt.xticks(size=11)
 plt.yticks(size=11)
-#plt.rcParams["figure.figsize"] = (9, 4)
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
 
 ax = sns.barplot(x="Layout", y="Runtime", data=data, hue="Datapoint", capsize=.1);
 ax.plot(np.nan, 'xr', label = 'Error')
+ax.plot(np.nan, 'xb', label = 'Error (Phasemarked)')
 
 ax.set_ylabel('Runtime (sec)', size=14)
 ax2 = ax.twinx()
 palette = {"Estimated":"tab:red",
+           "Estimated (Phasemarked)":"tab:blue",
            "Measured":"tab:blue"}
 sns.scatterplot(x="Layout", y="Error", ax=ax2, data=data, hue="Datapoint", palette=palette, s=100, marker="x", legend=False);
 #handlesr, labelsr = ax2.get_legend_handles_labels()
 #ax.legend(handles=(handles[0:] + handlesr[0:]), labels=(labels[0:] + labelsr[0:]))
 
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles=(handles[1:] + [handles[0]]), labels=(labels[1:] + [labels[0]]))
+ax.legend(handles=(handles[2:] + handles[0:2]), labels=(labels[2:] + labels[0:2]))
 
 ax2.grid(False)
 ax2.set_ylabel('Error (%)', size=14)
-ax2.set_ylim(0, data["Error"].max() * 1.3)
+ax2.set_ylim(0, data["Error"].max() * 1.5)
 if sys.argv[3] == "Perc":
     ax.set_xlabel('Percentage placed into HBM', size=14)
     ax2.set_xlabel('Percentage placed into HBM', size=14)
