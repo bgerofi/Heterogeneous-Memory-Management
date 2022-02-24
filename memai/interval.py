eb1bc882fad12a24c634b2f58cfb72717bd82e02
Nicolas Denoyelle <ndenoyelle@anl.gov>
2022-02-24 10:56:20 -0600

    Unit tests

--- memai/interval.py
+++ memai/interval.py
@@ -1,26 +1,55 @@
+import unittest
 import numpy as np
 from intervaltree import IntervalTree
 
 
 class IntervalDetector:
     def __init__(self, interval_distance=1 << 22, page_size=1 << 14):
-        self.intervals = IntervalTree()
+        self._intervals = IntervalTree()
         self._page_mask_ = ~(page_size - 1)
         self._interval_distance_ = interval_distance * page_size
 
     def append_addresses(self, vaddr):
         low_vaddr, high_vaddr = self._page_bounds_(vaddr)
         for low, high in zip(low_vaddr, high_vaddr):
-            self.intervals[low:high] = 0
-        self.intervals.merge_overlaps()
+            self._intervals[low:high] = 0
+        self._intervals.merge_overlaps(strict=False)
         return self
 
     def _page_bounds_(self, vaddr):
-        if isinstance(vaddr, list):
-            vaddr = np.array(vaddr)
-        if isinstance(vaddr, (np.ndarray, np.array)):
-            vaddr = vaddr.reshape(1, len(b))
         low = (vaddr & self._page_mask_) - self._interval_distance_
+        low = low.reshape((1, low.size))
         low = np.apply_along_axis(lambda i: max(i, 0), 0, low)
         high = (vaddr + self._interval_distance_) & self._page_mask_
         return low, high
+
+    @property
+    def intervals(self):
+        return self._intervals
+
+    def __iter__(self):
+        return iter(self._intervals)
+
+    def __len__(self):
+        return len(self._intervals)
+
+
+class TestIntervalDetector(unittest.TestCase):
+    def test_detector(self):
+        intervals = IntervalDetector(interval_distance=4, page_size=2)
+        addresses = np.array([0, 15, 30, 48])
+        intervals.append_addresses(addresses)
+        self.assertEqual(len(intervals), 2)
+
+        intervals = iter(sorted(intervals))
+        interval = next(intervals)
+        self.assertEqual(interval.begin, 0)
+        self.assertEqual(interval.end, 38)
+
+        interval = next(intervals)
+        self.assertEqual(interval.begin, 40)
+        self.assertEqual(interval.end, 56)
+
+
+if __name__ == "__main__":
+    unittest.main()
