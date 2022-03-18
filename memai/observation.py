c35d59646fc0a54d0f66f04626463c5db758ee7d
Nicolas Denoyelle <ndenoyelle@anl.gov>
2022-03-18 09:28:36 -0500

    Add unit testing.

--- memai/observation.py
+++ memai/observation.py
@@ -28,8 +28,8 @@ class WindowObservationSpace(Space):
 
     def from_sparse_matrix(self, x, y):
         def scale(x, low=0, high=1):
-            max_x = np.nanmax(y)
-            min_x = np.nanmin(y)
+            max_x = np.nanmax(x)
+            min_x = np.nanmin(x)
             x = np.array(x).copy()
 
             if max_x == min_x:
@@ -46,10 +46,10 @@ class WindowObservationSpace(Space):
             return x
 
         # Scale axis and convert to integer values in shape bounds.
-        y = scale(y, 0, self.shape[1]).astype(np.uint64)
-        y = np.minimum(y, self.shape[1] - 1)
-        x = scale(x, 0, self.shape[0]).astype(np.uint64)
-        x = np.minimum(x, self.shape[0] - 1)
+        y = scale(y, 0, self.shape[1] - 1)
+        x = scale(x, 0, self.shape[0] - 1)
+        y = np.round(y).astype(np.uint64)
+        x = np.round(x).astype(np.uint64)
 
         # Accumulate set values in empty matrix cells.
         ndarray = self.empty()
@@ -68,3 +68,93 @@ class WindowObservationSpace(Space):
             return len(x.shape) == 2
         else:
             return False
+
+
+class TestObservationSpace(unittest.TestCase):
+    def mat_to_coord(m):
+        x = []
+        y = []
+        for i in range(m.shape[0]):
+            for j in range(m.shape[1]):
+                if m[i, j] != 0:
+                    x.append(i)
+                    y.append(j)
+        return x, y
+
+    def test_scale_down(self):
+        observation_space = WindowObservationSpace(2, 2)
+        input_mat = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        check_mat = np.array([[2, 0], [0, 2]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+        ##############################
+
+        input_mat = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+        check_mat = np.array([[1, 1], [1, 0]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+        ##############################
+
+        observation_space = WindowObservationSpace(4, 4)
+        input_mat = np.array(
+            [
+                [0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0],
+            ]
+        )
+        check_mat = np.array([[0, 0, 0, 1], [0, 0, 2, 0], [0, 2, 0, 0], [1, 0, 0, 0]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+        ##############################
+
+        input_mat = np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+        check_mat = np.array([[0, 0, 1, 1], [0, 0, 1, 0], [1, 1, 1, 0], [1, 0, 0, 0]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+    def test_scale_up(self):
+        observation_space = WindowObservationSpace(4, 4)
+        input_mat = np.array([[0, 1], [1, 0]])
+        check_mat = np.array([[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+        ##############################
+
+        input_mat = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+        check_mat = np.array([[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0]])
+        output_mat = observation_space.from_sparse_matrix(
+            *TestObservationSpace.mat_to_coord(input_mat)
+        )
+        self.assertTrue(all(check_mat.flatten() == output_mat.flatten()))
+
+
+if __name__ == "__main__":
+    unittest.main()
